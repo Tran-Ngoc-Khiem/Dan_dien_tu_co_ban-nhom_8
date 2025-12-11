622527700e1d519303c9866e184a3456b6605eb3
Tran-Ngoc-Khiem <khiemtranzxc@gmail.com>
2025-12-12 05:45:35 +0700

    Delete design/sw/App/pwm_buzzer.h

--- design/sw/App/pwm_buzzer.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PWM_BUZZER_H
-#define __PWM_BUZZER_H
-
-#include "stm32f1xx_hal.h"
-
-// Hàm PWM_SetFrequency t? code g?c
-void PWM_SetFrequency(uint32_t freq);
-
-#endif /* __PWM_BUZZER_H */
