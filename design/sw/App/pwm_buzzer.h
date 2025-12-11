15aa1b7773ec29f257d520e543e600e0bc68badb
Tran-Ngoc-Khiem <khiemtranzxc@gmail.com>
2025-12-12 05:27:22 +0700

    Create pwm_buzzer.h

---
+++ design/sw/App/pwm_buzzer.h
@@ -0,0 +1,9 @@
+#ifndef __PWM_BUZZER_H
+#define __PWM_BUZZER_H
+
+#include "stm32f1xx_hal.h"
+
+// Hàm PWM_SetFrequency t? code g?c
+void PWM_SetFrequency(uint32_t freq);
+
+#endif /* __PWM_BUZZER_H */
